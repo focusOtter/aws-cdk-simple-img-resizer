4c2cbd7167bea10fa695c52cc367c5fcfd6a5bf5
focusotter <mtliendo@focusotter.com>
2024-01-24 01:56:13 -0600

    add deleting items after 30 days

--- lib/storage/originalBucket.ts
+++ lib/storage/originalBucket.ts
@@ -1,9 +1,14 @@
 import { Construct } from 'constructs'
 import { Bucket } from 'aws-cdk-lib/aws-s3'
-import { RemovalPolicy } from 'aws-cdk-lib'
+import { Duration, RemovalPolicy } from 'aws-cdk-lib'
 
 export const createOrignalImagesS3Bucket = (scope: Construct) => {
 	return new Bucket(scope, 'OrignalImagesS3Bucket', {
 		removalPolicy: RemovalPolicy.DESTROY,
+		lifecycleRules: [
+			{
+				expiration: Duration.days(30), // delete original items after 30 days
+			},
+		],
 	})
 }
